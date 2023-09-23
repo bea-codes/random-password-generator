82f6c9c9493ef3af04d0e83908cb4bf8c992e4b3
Beatriz <bia906@gmail.com>
2023-09-23 18:16:06 -0300

    Done

--- main.py
+++ main.py
@@ -3,32 +3,43 @@ Simple password generator built with Python.
 2023
 """
 
-letters_lowercase = "abcdefgijklmnopqrstuvwyxz"
-letter_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWYXZ"
-special_chars = "!@#$%*()|/?[]{\}"
-numbers ="1234567890"
+import random
+
+
+chars = [
+    "abcdefghijklmnopqrstuvwyxz",
+    "ABCDEFGHIJKLMNOPQRSTUVWYXZ",
+    "!@#$%*()|/?[]{}\\",
+    "1234567890",
+]
+
 
 def start():
-	print("Bea's Password Generator")
-	password_len = None
+    print("Bea's Password Generator")
+    password_len = None
 
-	while (isinstance(password_len, int) != True):
-		try:
-			password_len = int(input("Insert length for your password:\n"))
-		except KeyboardInterrupt:
-			break
-		except:
-				print("Please insert a number.\n")
-	generate_password()
-
-def generate_password():
-	print("Generating your password...")
-	password_len = 10 # Tamanho da senha arbirtrário para fins de desenvolvimento...
-	
+    while isinstance(password_len, int) != True:
+        try:
+            password_len = int(input("Insert length for your password:\n"))
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Please insert a number.\n")
+    generate_password(password_len)
 
 
+def generate_password(password_len):
+    print("Generating your password...")
+    list_password = []
+    for c in range(password_len):
+        type_of_char = chars[random.randrange(4)]
+        len_type_of_char = len(type_of_char)
+        c = type_of_char[random.randrange(len_type_of_char)]
+        list_password.append(c)
 
+    password = "".join(list_password)
+    print(f"Here's your new password:\n{password}")
 
 
 if __name__ == "__main__":
-	start()
+    start()
